0e58dbf249c5683f324100e926f58f8524eb1594
Jan Čapek <jan.capek@braiins.cz>
2018-09-22 01:47:00 +0200

    [docker] Fixup owner of the release-build directory (from root to local user)

--- docker/build-release-as-local-uid.sh
+++ docker/build-release-as-local-uid.sh
@@ -3,7 +3,8 @@
 
 addgroup --gid=$LOC_GID build
 adduser --system --home=$RELEASE_BUILD_DIR --no-create-home --uid=$LOC_UID --gid=$LOC_GID build
-sudo -E -u build bash << EOF
-export HOME=$RELEASE_BUILD_DIR
+# release build is already created due to the fact that we have mapped into it .ssh with known hosts file
+chown build.build $RELEASE_BUILD_DIR
+sudo -H -E -u build bash << EOF
 ./build-release.sh $@
 EOF
