abfc5a377116286b3b08341a47459a73401c7d58
Jan Čapek <jan.capek@braiins.cz>
2018-09-22 00:44:37 +0200

    [docker] Implement RELEASE_BUILD_DIR for specify build user HOME directory

--- docker/build-release-as-local-uid.sh
+++ docker/build-release-as-local-uid.sh
@@ -2,7 +2,8 @@
 # Purpose: custom script that prepares user environment and execute the release build
 
 addgroup --gid=$LOC_GID build
-adduser --system --home=/ --no-create-home --uid=$LOC_UID --gid=$LOC_GID build
-sudo -u build bash << EOF
+adduser --system --home=$RELEASE_BUILD_DIR --no-create-home --uid=$LOC_UID --gid=$LOC_GID build
+sudo -E -u build bash << EOF
+export HOME=$RELEASE_BUILD_DIR
 ./build-release.sh $@
 EOF
--- docker/build-release-in-docker.sh
+++ docker/build-release-in-docker.sh
@@ -7,5 +7,6 @@ echo params: $@
 #docker run --rm -u `id -u`:`id -u` -v ${PWD}:/src -w /src \
 #       braiins-os-builder ./build-release.sh $@
 
-docker run --env LOC_UID=`id -u` --env LOC_GID=`id -g` --volume $HOME/.ssh/known_hosts:/.ssh/known_hosts:ro --volume $SSH_AUTH_SOCK:/ssh-agent --volume\
+release_build_dir=/src/release-build
+docker run --env RELEASE_BUILD_DIR=$release_build_dir --env LOC_UID=`id -u` --env LOC_GID=`id -g` --volume $HOME/.ssh/known_hosts:$release_build_dir/.ssh/known_hosts:ro --volume $SSH_AUTH_SOCK:/ssh-agent --volume\
  ${PWD}:/src -w /src --env SSH_AUTH_SOCK=/ssh-agent braiins-os-builder ./docker/build-release-as-local-uid.sh $@
